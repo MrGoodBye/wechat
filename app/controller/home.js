41c17254ef3bb96944eaf1b8598e2dfc8c0f18e8
MrGoodBye <1254993267@qq.com>
2018-02-03 10:19:48 +0800

    test sha1

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,6 +2,7 @@
 
 const Controller = require('egg').Controller
 const sign = require('../utils/sign')
+const TOKEN = 'mrgoodbye'
 
 class HomeController extends Controller {
     async index() {
@@ -9,10 +10,12 @@ class HomeController extends Controller {
     }
 
     async test() {
-        // const params = this.ctx.request.body
-        console.log(this.ctx.query)
-        console.log(sign('jsapi_ticket', this.ctx.url))
-        this.ctx.body = this.ctx
+        const {signature, echostr, timestamp, nonce} = this.ctx.request.body
+        const key = [TOKEN, timestamp, nonce].sort().join('')
+        const sign = require('crypto').createHash('sha1').update(key).digest('hex')
+        console.log(sign === signature)
+        // console.log(sign('jsapi_ticket', this.ctx.url))
+        this.ctx.body = this.ctx.query
     }
 }
 
