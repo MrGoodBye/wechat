eb4a59207e864ddd6f9c9a0eddaf9190310832f9
MrGoodBye <1254993267@qq.com>
2018-02-25 10:29:56 +0800

    fix conflict

--- app/router.js
+++ app/router.js
@@ -6,5 +6,5 @@
 module.exports = app => {
   const { router, controller } = app
   router.get('/', controller.home.index)
-  router.get('/wx', controller.home.index)
+  router.get('/wx', controller.home.test)
 }
